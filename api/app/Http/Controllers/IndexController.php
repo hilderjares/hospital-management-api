5601f1b712f9c94657df5c7da8f1bd0df0bc6468
hilderjares <hider1195@gmail.com>
2020-03-29 03:50:30 -0300

    project initial structure

---
+++ api/app/Http/Controllers/IndexController.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Response;
+
+class IndexController extends Controller
+{
+    public function index(): JsonResponse
+    {
+        return Response()->json(['message' => 'documentation']);
+    }
+}
---
+++ api/database/migrations/2020_03_29_061820_create_hospital_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHospitalTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hospital', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->text('address');
+            $table->string('city');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hospital');
+    }
+}
